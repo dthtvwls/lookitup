e0d2eec17d0c0871beba258d8049581ff29c1fdb
Joshua Stauter <joshua@localhost.local>
2014-11-21 16:19:31 -0500

    native map

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (isset($_GET['async'])) {
   <body>
     <div id="chart"></div>
     <script src="http://coffeescript.org/extras/coffee-script.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.underscore.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/highstock/2.0.4/highstock.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/highcharts/4.0.4/themes/grid.js"></script>
@@ -30,7 +30,7 @@ if (isset($_GET['async'])) {
               $.getJSON '?async=x:NYSEARCA,p:40Y,i:86400,q:' + symbol, (json)->
                 chart.addSeries
                   name: json['n'][0]
-                  data: _.zip _.map(json['t'], (t)-> Date.parse t), json['v'][0]
+                  data: _.zip json['t'].map((t)-> Date.parse t), json['v'][0]
     </script>
   </body>
 </html>
