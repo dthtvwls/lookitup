6da71be3d245f870301b623c2b00bc3723269d67
dthtvwls <dthtvwls@users.noreply.github.com>
2016-03-20 21:58:40 -0400

    update

--- index.php
+++ index.php
@@ -1,36 +1,40 @@
 <?php
-if (isset($_GET['async'])) {
-  $url = 'http://www.google.com/async/finance_chart_data?async=' . $_GET['async'];
+if (isset($_GET['symbol'])) {
+  $url = 'http://www.google.com/async/finance_chart_data?async=x:MUTF,p:40Y,i:86400,q:' . $_GET['symbol'];
   die(json_decode(explode("\n", file_get_contents($url))[1], true)['tnv']['value']);
 }
-?>
-<!doctype html>
+?><!doctype html>
 <html>
   <head>
-    <title>Index Fund Performance</title>
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css">
+    <title>Fund Performance</title>
+    <link href="//cdnjs.cloudflare.com/ajax/libs/normalize/4.0.0/normalize.min.css" rel="stylesheet">
     <style>html, body, #chart { height: 100%; }</style>
   </head>
   <body>
     <div id="chart"></div>
     <script src="http://coffeescript.org/extras/coffee-script.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/highstock/2.0.4/highstock.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/highcharts/4.0.4/themes/grid.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/4.6.1/lodash.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highcharts/4.2.3/highcharts.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highcharts/4.2.3/themes/grid.js"></script>
     <script type="text/coffeescript">
-    $('#chart').highcharts 'StockChart',
-      chart:
-        zoomType: 'xy'
-        events:
-          load: ->
-            chart = this
+      $('#chart').highcharts
+        credits: enabled: false
+        title: null
+        tooltip: valueDecimals: 2
+        xAxis: type: 'datetime'
+        yAxis: title: null
+        chart:
+          zoomType: 'xy'
+          events:
+            load: ->
+              chart = @
 
-            ['VTI', 'VEA', 'VWO', 'VIG', 'VNQ', 'LQD', 'EMB'].forEach (symbol)->
-              $.getJSON '?async=x:NYSEARCA,p:40Y,i:86400,q:' + symbol, (json)->
-                chart.addSeries
-                  name: json['n'][0]
-                  data: _.zip json['t'].map((t)-> Date.parse t), json['v'][0]
+              ['VEXRX'].forEach (symbol) ->
+                $.getJSON '?symbol=' + symbol, (json) ->
+                  chart.addSeries
+                    name: json['n'][0]
+                    data: _.zip json['t'].map((t) -> Date.parse t), json['v'][0]
     </script>
   </body>
 </html>
