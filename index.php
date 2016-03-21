01751e96c22a4b43a69cacfbf3a60fd7dd7f100a
dthtvwls <dthtvwls@users.noreply.github.com>
2016-03-20 23:58:47 -0400

    caching

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 if (isset($_GET['symbol'])) {
-  $url = 'http://www.google.com/async/finance_chart_data?async=x:MUTF,p:40Y,i:86400,q:' . $_GET['symbol'];
-  die(json_decode(explode("\n", file_get_contents($url))[1], true)['tnv']['value']);
+  require __DIR__ . '/vendor/autoload.php';
+  $redis = isset($_ENV['REDIS_URL']) ? new Predis\Client($_ENV['REDIS_URL']) : new Predis\Client;
+  if (!($data = $redis->get($_GET['symbol']))) {
+    $data = json_decode(explode("\n", file_get_contents('http://www.google.com/async/finance_chart_data?async=x:MUTF,p:40Y,i:86400,q:' . $_GET['symbol']))[1], true)['tnv']['value'];
+    $redis->set($_GET['symbol'], $data);
+    $redis->expire($_GET['symbol'], 86400);
+  }
+  die($data);
 }
 ?><!doctype html>
 <html>
@@ -28,13 +34,32 @@ if (isset($_GET['symbol'])) {
           zoomType: 'xy'
           events:
             load: ->
+              funds =
+                VFIDX: 'Interm Term Investment Grade Bond Adm'
+                VTAPX: 'Short Term Inflation Protection Securites Index Adml'
+                VFSUX: 'Short Term Investment Grade Adml'
+                VBTLX: 'Total Bond Market Index Adml'
+                VTABX: 'Total International Bond Index Adml'
+                VFIAX: '500 Index Adml'
+                VBIAX: 'Balanced Index Adml'
+                VTMGX: 'Developed Markets Index Adm'
+                VEMAX: 'Emerging Markets Stock Index Adml'
+                VEXRX: 'Explorer Adm'
+                VSCGX: 'Lifestrategy Conservative Growth Inv'
+                VASGX: 'Lifestrategy Growth Inv'
+                VASIX: 'Lifestrategy Income Inv'
+                VSMGX: 'Lifestrategy Moderate Growth Inv'
+                VIMAX: 'Mid Cap Index Fund Adml'
+                VMRAX: 'Morgan Growth Adm'
+                VSMAX: 'Small Cap Index Adml'
+                VTIAX: 'Total International Stock Index Adml'
+                VTSAX: 'Total Stock Market Index Adml'
+                VWNFX: 'Windsor Ii Inv'
+
               chart = @
 
-              ['VEXRX'].forEach (symbol) ->
-                $.getJSON '?symbol=' + symbol, (json) ->
-                  chart.addSeries
-                    name: json['n'][0]
-                    data: _.zip json['t'].map((t) -> Date.parse t), json['v'][0]
+              Object.keys(funds).forEach (symbol) -> $.getJSON '?symbol=' + symbol, (json) ->
+                chart.addSeries name: funds[json['n'][0]], data: _.zip json['t'].map((t) -> Date.parse t), json['v'][0]
     </script>
   </body>
 </html>
