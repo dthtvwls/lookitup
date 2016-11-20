eb67fc3d2faee3392140dbe7bff619bfc1be132a
dthtvwls <dthtvwls@users.noreply.github.com>
2016-11-20 00:18:37 -0500

    cors try 1

--- index.php
+++ index.php
@@ -5,10 +5,12 @@ if (preg_match('/^[A-Z]{1,5}$/', $symbol)) {
   require __DIR__ . '/vendor/autoload.php';
 
   $client = new Predis\Client($_ENV['REDIS_URL']);
+
   $data = $client->get($symbol);
 
   if ($data === null) {
     $matches = json_decode(file_get_contents("http://www.google.com/finance/match?q=$symbol"))->matches;
+
     if (count($matches)) {
       $data = json_decode(json_decode(explode("\n", file_get_contents(
         "http://www.google.com/async/finance_chart_data?async=x:{$matches[0]->e},p:40Y,i:86400,q:$symbol"
@@ -27,42 +29,7 @@ if (preg_match('/^[A-Z]{1,5}$/', $symbol)) {
   }
 
   header('Content-Encoding: gzip');
-  die($data);
+  header('Access-Control-Allow-Origin: *');
+  // add_header Access-Control-Allow-Headers X-Requested-With;
+  echo $data;
 }
-?><!doctype html>
-<html>
-  <head>
-    <title>data</title>
-    <style>
-      html, body { height: 100%; }
-      body { margin: 0; }
-    </style>
-  </head>
-  <body>
-    <script src="//code.highcharts.com/4.2/highcharts.js"></script>
-    <script src="//code.highcharts.com/4.2/themes/grid.js"></script>
-    <script>
-      new Highcharts.Chart({
-        credits: { enabled: false },
-        title: null,
-        tooltip: { valueDecimals: 2 },
-        xAxis: { type: 'datetime' },
-        yAxis: { title: null },
-        chart: {
-          renderTo: document.body,
-          zoomType: 'xy',
-          events: {
-            load: function () {
-              location.search.slice(1).split(',').forEach(symbol => {
-                const xhr = new XMLHttpRequest()
-                xhr.addEventListener('load', () => this.addSeries({ name: symbol, data: JSON.parse(xhr.responseText) }))
-                xhr.open('GET', '/?' + symbol)
-                xhr.send()
-              })
-            }
-          }
-        }
-      })
-    </script>
-  </body>
-</html>
