c8560e8e77f88d857b10c3782aab9261fb44adc0
dthtvwls <dthtvwls@users.noreply.github.com>
2017-07-24 11:03:39 -0400

    use google finance endpoint

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
-function matches($q) {
-  return json_decode(file_get_contents("https://www.google.com/finance/match?q=$q"))->matches;
-}
-
 if (isset($_GET['q'])) {
-  echo json_encode(matches($_GET['q']));
+  echo json_encode(json_decode(file_get_contents("https://www.google.com/finance/match?q=${_GET['q']}"))->matches);
 
-} else if (array_key_exists('QUERY_STRING', $_SERVER) && preg_match('/^[A-Z]{1,5}$/', $_SERVER['QUERY_STRING'])) {
+} else if (isset($_SERVER['QUERY_STRING']) && preg_match('/^[A-Z]{1,5}$/', $_SERVER['QUERY_STRING'])) {
   require __DIR__ . '/vendor/autoload.php';
 
   date_default_timezone_set('UTC');
@@ -16,23 +12,24 @@ if (isset($_GET['q'])) {
   $data = $client->get($symbol);
 
   if ($data === null) {
-    $matches = matches($symbol);
+    $i = 604800;
+    $file = file_get_contents("https://www.google.com/finance/getprices?i=$i&p=40Y&f=d,c&q=${_SERVER['QUERY_STRING']}");
+    $output = [];
 
-    if (count($matches) > 0) {
-      $data = json_decode(json_decode(explode("\n", file_get_contents(
-        "https://www.google.com/async/finance_chart_data?async=x:{$matches[0]->e},p:40Y,i:86400,q:$symbol"
-      ))[1])->tnv->value);
-
-      // array_map with null first argument works like array zip in more normal languages
-      $data = gzencode(json_encode(array_map(null,
-        // Highcharts doesn't understand the time unless we give it unix timestamp with milliseconds
-        array_map(function ($t) { return strtotime($t) * 1000; }, $data->t),
-        $data->v[0]
-      )), 9);
-
-      $client->set($symbol, $data);
-      $client->expire($symbol, 86400);
+    foreach (explode("\n", $file) as $line) {
+      if ($line[0] === 'a') {
+        $pair = explode(',', $line);
+        $anchor = ltrim($pair[0], 'a');
+        array_push($output, [$anchor * 1000, intval($pair[1])]);
+      } else if (is_numeric($line[0])) {
+        $pair = explode(',', $line);
+        array_push($output, [($anchor + $pair[0] * $i) * 1000, intval($pair[1])]);
+      }
     }
+
+    $data = gzencode(json_encode($output), 9);
+    $client->set($symbol, $data);
+    $client->expire($symbol, 86400);
   }
 
   header('Content-Encoding: gzip');
